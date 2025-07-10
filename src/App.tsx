7bdf46bd3a406560d492616cb3ba23fc147861f7
manhhoach <manhhn.nv5@gmail.com>
2025-07-10 22:04:26 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import MapView from './components/MapView';
 import { useState } from 'react';
 import Coordinates from './types/Coordinates';
 
-
 function App() {
   const [selectedCoords, setSelectedCoords] = useState<Coordinates | null>(null);
+
   return (
     <Row style={{ height: '100vh' }}>
       <Col span={5}>
--- src/components/DestinationItem.tsx
+++ src/components/DestinationItem.tsx
@@ -6,18 +6,27 @@ import Coordinates from "../types/Coordinates";
 interface DestinationItemProps {
    tour: TourDto;
    onSelect: (coords: Coordinates) => void;
+   setSelectedTour: (tour: TourDto | null) => void;
+   isCurrent: boolean;
 }
 
 
-const DestinationItem: FC<DestinationItemProps> = ({ tour, onSelect }) => {
+const DestinationItem: FC<DestinationItemProps> = ({ tour, onSelect, setSelectedTour, isCurrent }) => {
+   const handleClick = () => {
+      setSelectedTour(tour);
+      onSelect(tour.coordinates);
+   }
    return (
       <Card
          hoverable
          cover={<img alt={tour.name} src={tour.imageUrl} />}
-         onClick={() => onSelect(tour.coordinates)}
+         onClick={() => handleClick()}
          style={{ marginBottom: '1rem' }}
       >
          <Card.Meta title={tour.name} description={tour.location} />
+         {
+            isCurrent && <div style={{ color: 'green', fontWeight: 'bold' }}>Current Destination</div>
+         }
       </Card>
    );
 };
