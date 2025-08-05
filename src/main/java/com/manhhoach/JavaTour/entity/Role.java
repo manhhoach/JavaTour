ccad8fc9c6a061957b65c67d894952e1c70f5bc7
BUI THE MANH Assistant/(Part) Public Sector <buithemanh@lgcns.com>
2025-08-05 08:21:04 +0700

    reg user

--- src/main/java/com/manhhoach/JavaTour/entity/Role.java
+++ src/main/java/com/manhhoach/JavaTour/entity/Role.java
@@ -1,15 +1,19 @@
 package com.manhhoach.JavaTour.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Set;
 
 @Entity
 @Data
+@AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class Role extends BaseEntity{
     private String name;
     private String code;
